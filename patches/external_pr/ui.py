606e8635fc879116305ce672f0e12fe28cfc8099
AngelBottomless <35677394+aria1th@users.noreply.github.com>
2023-02-15 01:17:36 +0900

    Beta dadaptation (#45)
    * Beta-Dadaptation
    
    Based on https://github.com/facebookresearch/dadaptation/blob/main/dadaptation/dadapt_adam.py

--- patches/external_pr/ui.py
+++ patches/external_pr/ui.py
@@ -83,7 +83,7 @@ def save_training_setting(*args):
     gamma_rate, use_beta_adamW_checkbox, save_when_converge, create_when_converge, \
     adamw_weight_decay, adamw_beta_1, adamw_beta_2, adamw_eps, show_gradient_clip_checkbox, \
     gradient_clip_opt, optional_gradient_clip_value, optional_gradient_norm_type, latent_sampling_std,\
-    noise_training_scheduler_enabled, noise_training_scheduler_repeat, noise_training_scheduler_cycle, loss_opt = args
+    noise_training_scheduler_enabled, noise_training_scheduler_repeat, noise_training_scheduler_cycle, loss_opt, use_dadaptation = args
     dumped_locals = locals()
     dumped_locals.pop('args')
     filename = (str(random.randint(0, 1024)) if save_file_name == '' else save_file_name) + '_train_' + '.json'
@@ -140,6 +140,7 @@ def on_train_gamma_tab(params=None):
             show_noise_options = gr.Checkbox(
                 label='Show Noise Scheduler Options(for both)')
         with gr.Row(visible=False) as adamW_options:
+            use_dadaptation = gr.Checkbox(label="Uses D-Adaptation(LR Free) AdamW. Recommended LR is 1.0 at base")
             adamw_weight_decay = gr.Textbox(label="AdamW weight decay parameter", placeholder="default = 0.01",
                                             value="0.01")
             adamw_beta_1 = gr.Textbox(label="AdamW beta1 parameter", placeholder="default = 0.9", value="0.9")
@@ -273,7 +274,8 @@ def on_train_gamma_tab(params=None):
         noise_training_scheduler_enabled,
         noise_training_scheduler_repeat,
         noise_training_scheduler_cycle,
-        loss_opt],
+        loss_opt,
+        use_dadaptation],
         outputs=[
             ti_output,
             ti_outcome,
@@ -372,8 +374,9 @@ def on_train_gamma_tab(params=None):
         noise_training_scheduler_enabled,
         noise_training_scheduler_repeat,
         noise_training_scheduler_cycle,
-            load_training_option
-
+            load_training_option,
+            loss_opt,
+            use_dadaptation
         ],
         outputs=[
             ti_output,
